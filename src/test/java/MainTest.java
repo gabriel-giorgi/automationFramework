b6c6c0f4d18d0fd74591bfc55d6eb7deaa4023a5
Gabriel Giorgi <gabigior93@gmail.com>
2017-05-04 11:32:25 -0300

    Adding a test class

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,23 @@
+/**
+ * Created by ggiorgi on 4/25/2017.
+ */
+
+import org.openqa.selenium.WebDriver;
+import utils.BaseClass;
+
+
+public class MainTest extends BaseClass {
+    WebDriver driver;
+
+    /*
+    @Title("First allure report test")
+    @Description("Here we are goint to test for first time if allure report work correctly")
+    @Test(testName = "Test allure report")
+    public void testLogs() {
+        Assert.assertTrue(2*2==4);
+        System.out.println("This is a test");
+    }
+    */
+
+}
+
