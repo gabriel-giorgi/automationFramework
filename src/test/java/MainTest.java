43c43a177965d8c47ed7659cedd59c295eefa2b7
Gabriel Giorgi <gabigior93@gmail.com>
2017-05-04 12:22:49 -0300

    Implemented Allure report Framework

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,14 +2,19 @@
  * Created by ggiorgi on 4/25/2017.
  */
 
+
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Title;
 import utils.BaseClass;
 
 
 public class MainTest extends BaseClass {
     WebDriver driver;
 
-    /*
+
     @Title("First allure report test")
     @Description("Here we are goint to test for first time if allure report work correctly")
     @Test(testName = "Test allure report")
@@ -17,7 +22,6 @@ public class MainTest extends BaseClass {
         Assert.assertTrue(2*2==4);
         System.out.println("This is a test");
     }
-    */
 
 }
 
